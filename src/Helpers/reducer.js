2977f708c2df5fe3d7622c820392c01767139e1a
Mocha Adam Fadly <adamfadly11@gmail.com>
2021-03-18 13:50:41 +0700

    header component with dynamic data

--- src/Helpers/reducer.js
+++ src/Helpers/reducer.js
@@ -3,7 +3,7 @@ export const initialState = {
   // null,
   // "BQBADFI4GF1PEvjvWdmDNkHpq0QBiA62nw-TByPRhDOoOpFYUe0sJqVAK8RW2Ey-LcToMjaJ2-ZH0y7GazgTL_iwQUtfNJ1LBXCeuh3mkPaWM5nTFdy7S3rQDBL-b_9NIMvnM3icRaF_xF9VpVur8Cey7Y5_S9c",
   user: null,
-  playlists: [],
+  playlists: "",
   playing: false,
   item: null,
 };
